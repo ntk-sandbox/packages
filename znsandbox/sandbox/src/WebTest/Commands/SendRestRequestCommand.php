a1ccb7814940da5e0b3e09e0b14bde097a3de152
vitaliy <zndev@ya.ru>
2023-01-07 19:32:17 +0600

    upd

--- znsandbox/sandbox/src/WebTest/Commands/SendRestRequestCommand.php
+++ znsandbox/sandbox/src/WebTest/Commands/SendRestRequestCommand.php
@@ -8,8 +8,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelBrowser;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use ZnCore\Collection\Libs\Collection;
 use ZnCore\Contract\Encoder\Interfaces\EncoderInterface;
 use ZnFramework\Console\Domain\Libs\ZnShell;
+use ZnLib\Components\Format\Encoders\ChainEncoder;
+use ZnLib\Components\Format\Encoders\PhpSerializeEncoder;
+use ZnLib\Components\Format\Encoders\SafeBase64Encoder;
 
 //use Symfony\Component\BrowserKit\HttpBrowser;
 
@@ -72,8 +76,11 @@ class SendRestRequestCommand extends BaseCommand
 
 class ConsoleHttpKernel implements HttpKernelInterface {
     
-    public function __construct(private EncoderInterface $encoder)
+    protected EncoderInterface $encoder;
+    
+    public function __construct()
     {
+        $this->encoder = $this->createEncoder();
     }
 
     public function handle(Request $request, int $type = self::MAIN_REQUEST, bool $catch = true): Response
@@ -94,5 +101,18 @@ class ConsoleHttpKernel implements HttpKernelInterface {
             ]
         )->getOutput();
         return $encodedResponse;
+    }
+
+    protected function createEncoder()
+    {
+        $requestEncoder = new ChainEncoder(
+            new Collection(
+                [
+                    new PhpSerializeEncoder(),
+                    new SafeBase64Encoder(),
+                ]
+            )
+        );
+        return $requestEncoder;
     }
 }
