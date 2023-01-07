9c08ca4898c32787eacf7061c698daedba31fa0f
vitaliy <zndev@ya.ru>
2023-01-07 18:24:30 +0600

    upd

--- znsandbox/sandbox/src/WebTest/Commands/SendRestRequestCommand.php
+++ znsandbox/sandbox/src/WebTest/Commands/SendRestRequestCommand.php
@@ -2,24 +2,11 @@
 
 namespace ZnSandbox\Sandbox\WebTest\Commands;
 
-use App\Application\Admin\Libs\AdminApp;
-use App\Application\Rpc\Libs\RpcApp;
-use App\Application\Web\Libs\WebApp;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use ZnCore\Collection\Libs\Collection;
-use ZnCore\Container\Helpers\ContainerHelper;
-use ZnCrypt\Base\Domain\Helpers\SafeBase64Helper;
-use ZnCrypt\Base\Domain\Libs\Encoders\CollectionEncoder;
 use ZnFramework\Console\Domain\Libs\ZnShell;
-use ZnLib\Components\Format\Encoders\ChainEncoder;
-use ZnLib\Components\Format\Encoders\PhpSerializeEncoder;
-use ZnLib\Components\Format\Encoders\SafeBase64Encoder;
-use ZnSandbox\Sandbox\WebTest\Domain\Libs\AppFactory;
-use ZnSandbox\Sandbox\WebTest\Domain\Libs\JsonHttpClient;
-use ZnSandbox\Sandbox\WebTest\Domain\Libs\HttpClient;
 
 class SendRestRequestCommand extends BaseCommand
 {
@@ -28,10 +15,6 @@ class SendRestRequestCommand extends BaseCommand
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $output->writeln('<fg=white># Send request</>');
-
-        $httpClient = $this->createHttpClient();
-
         $jsonRpcRequest = [
             "jsonrpc" => "2.0",
             "method" => "authentication.getTokenByPassword",
@@ -43,18 +26,21 @@ class SendRestRequestCommand extends BaseCommand
             ],
         ];
 
+        $httpClient = $this->createHttpClient();
         $request = $httpClient->createRequest('POST', '/json-rpc', $jsonRpcRequest);
-
-        $requestEncoder = $this->createEncoder();
-
-        $encodedRequest = $requestEncoder->encode($request);
-
-        $encodedResponse = $this->runConsoleCommand($encodedRequest);
-
-        $response = $requestEncoder->decode($encodedResponse);
-
+        $response = $this->handleRequest($request);
         $output->writeln($response->getContent());
+
         return 0;
+    }
+
+    protected function handleRequest(Request $request): Response
+    {
+        $requestEncoder = $this->createEncoder();
+        $encodedRequest = $requestEncoder->encode($request);
+        $encodedResponse = $this->runConsoleCommand($encodedRequest);
+        $response = $requestEncoder->decode($encodedResponse);
+        return $response;
     }
 
     protected function runConsoleCommand(string $encodedRequest): string
