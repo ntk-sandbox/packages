9e9878cc0a5911570f06bdb1d5c762a5de0894ea
vitaliy <zndev@ya.ru>
2023-01-20 14:45:50 +0600

    upd

---
+++ zncore/app/src/Libs/BaseEnvironment.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace ZnCore\App\Libs;
+
+use ZnCore\App\Interfaces\EnvironmentInterface;
+use ZnCore\Container\Traits\ContainerAwareTrait;
+use ZnCore\DotEnv\Domain\Libs\DotEnvResolver;
+use ZnCore\DotEnv\Domain\Libs\DotEnvWriter;
+
+abstract class BaseEnvironment implements EnvironmentInterface
+{
+
+    use ContainerAwareTrait;
+
+    abstract public function init(string $mode = null): void;
+
+    protected function loadVars(array $env): void
+    {
+        $env = $this->getResolver()->resolve($env);
+        $this->getWriter()->setAll($env);
+    }
+
+    protected function getResolver(): DotEnvResolver
+    {
+        /** @var DotEnvResolver $resolver */
+        $resolver = $this->getContainer()->get(DotEnvResolver::class);
+        return $resolver;
+    }
+
+    protected function getWriter(): DotEnvWriter
+    {
+        /** @var DotEnvWriter $writer */
+        $writer = $this->getContainer()->get(DotEnvWriter::class);
+        return $writer;
+    }
+}
