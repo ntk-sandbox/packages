c7c5bcae181742c71b3b8ca933928fd9ea4a0b88
vitaliy <zndev@ya.ru>
2023-01-21 23:48:32 +0600

    upd

--- znsandbox/sandbox/src/WebTest/Domain/Libs/ConsoleHttpKernel.php
+++ znsandbox/sandbox/src/WebTest/Domain/Libs/ConsoleHttpKernel.php
@@ -5,8 +5,10 @@ namespace ZnSandbox\Sandbox\WebTest\Domain\Libs;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Process\Process;
 use ZnCore\Collection\Libs\Collection;
 use ZnCore\Contract\Encoder\Interfaces\EncoderInterface;
+use ZnFramework\Console\Domain\Helpers\CommandLineHelper;
 use ZnFramework\Console\Domain\Libs\ZnShell;
 use ZnLib\Components\Format\Encoders\ChainEncoder;
 use ZnLib\Components\Format\Encoders\PhpSerializeEncoder;
@@ -32,14 +34,27 @@ class ConsoleHttpKernel implements HttpKernelInterface
 
     protected function runConsoleCommand(string $encodedRequest): string
     {
-        $shell = new ZnShell();
+        $command = [
+            'php',
+            'isolated',
+            'http:request:run',
+            "--factory-class" => \App\Application\Common\Factories\HttpKernelFactory::class,
+            $encodedRequest
+        ];
+        $commandString = CommandLineHelper::argsToString($command);
+        $process = Process::fromShellCommandline($commandString, realpath(__DIR__ . '/../../../../../../znsandbox/sandbox/src/WebTest/resouces/bin'));
+        $res = $process->run();
+        $encodedResponse = $process->getOutput();
+
+        /*$shell = new ZnShell();
         $encodedResponse = $shell->runProcess(
             [
                 'http:request:run',
                 "--factory-class" => \App\Application\Common\Factories\HttpKernelFactory::class,
                 $encodedRequest
             ]
-        )->getOutput();
+        )->getOutput();*/
+
         return $encodedResponse;
     }
 
