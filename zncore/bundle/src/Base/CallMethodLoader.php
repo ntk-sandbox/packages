098fa81df2bd0e900c97615edd5e84915039918a
vitaliy <zndev@ya.ru>
2023-02-02 23:34:16 +0600

    upd

--- zncore/bundle/src/Base/CallMethodLoader.php
+++ zncore/bundle/src/Base/CallMethodLoader.php
@@ -7,7 +7,6 @@ class CallMethodLoader extends BaseLoader
 
     public function loadAll(array $bundles): void
     {
-        $config = [];
         foreach ($bundles as $bundle) {
             $this->callMethod($bundle);
         }
