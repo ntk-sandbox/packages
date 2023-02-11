14193e6bf4d9bb5534d335cbfb03c9fd3d2153e0
vitaliy <zndev@ya.ru>
2023-02-11 23:11:37 +0600

    upd

--- untek-domain/domain/src/Interfaces/DomainInterface.php
+++ untek-domain/domain/src/Interfaces/DomainInterface.php
@@ -2,7 +2,10 @@
 
 namespace Untek\Domain\Domain\Interfaces;
 
+use Untek\Core\Code\Helpers\DeprecateHelper;
 use Untek\Core\Contract\Common\Interfaces\GetNameInterface;
+
+DeprecateHelper::hardThrow();
 
 interface DomainInterface extends GetNameInterface
 {
--- untek-domain/query/src/Helpers/QueryHelper.php
+++ untek-domain/query/src/Helpers/QueryHelper.php
@@ -3,7 +3,10 @@
 namespace Untek\Domain\Query\Helpers;
 
 use Untek\Core\Arr\Helpers\ArrayHelper;
+use Untek\Core\Code\Helpers\DeprecateHelper;
 use Untek\Domain\Query\Entities\Query;
+
+DeprecateHelper::hardThrow();
 
 class QueryHelper
 {
