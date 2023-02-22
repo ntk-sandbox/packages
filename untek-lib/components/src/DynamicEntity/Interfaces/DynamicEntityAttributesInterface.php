3a525070c93f7fb4b86e96e0e02db8deb26dab4c
vitaliy <zndev@ya.ru>
2023-02-22 14:23:57 +0600

    upd

--- untek-lib/components/src/DynamicEntity/Interfaces/DynamicEntityAttributesInterface.php
+++ untek-lib/components/src/DynamicEntity/Interfaces/DynamicEntityAttributesInterface.php
@@ -2,6 +2,11 @@
 
 namespace Untek\Lib\Components\DynamicEntity\Interfaces;
 
+/**
+ * Interface DynamicEntityAttributesInterface
+ * @package Untek\Lib\Components\DynamicEntity\Interfaces
+ * @deprecated 
+ */
 interface DynamicEntityAttributesInterface
 {
 
