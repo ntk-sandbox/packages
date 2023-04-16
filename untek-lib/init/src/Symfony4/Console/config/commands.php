25086e8da75a3403e8b8980c2d1c3eb597c16437
vitaliy <zndev@ya.ru>
2023-04-16 22:57:06 +0600

    upd

--- untek-lib/init/src/Symfony4/Console/config/commands.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Untek\Framework\Console\Symfony4\Interfaces\CommandConfiguratorInterface;
-use Untek\Lib\Init\Symfony4\Console\Commands\InitCommand;
-
-return function (CommandConfiguratorInterface $commandConfigurator) {
-    $commandConfigurator->registerCommandClass(InitCommand::class);
-};
